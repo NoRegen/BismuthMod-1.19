1063ba842068244be167957c388f7ba1974ef5ef
NoRegen <Leko.030307@gmail.com>
2023-06-26 00:59:00 -0700

    Project Set Up!

---
+++ src/main/java/net/noregen/bismuth/Bismuth.java
@@ -0,0 +1,15 @@
+package net.noregen.bismuth;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Bismuth implements ModInitializer {
+	public static final String MOD_ID = "bismuth";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/noregen/bismuth/BismuthClient.java
@@ -0,0 +1,10 @@
+package net.noregen.bismuth;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BismuthClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
